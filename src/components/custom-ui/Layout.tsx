5986f9c80429141e26f1a0b83e57a41608e5445d
XIV-Y <yutodazepa.g@gmail.com>
2025-08-27 00:42:21 +0900

    #12 refactor: ページ全体にヘッダーとフッターの高さを含めないmin-hを設定

--- src/components/custom-ui/Layout.tsx
+++ src/components/custom-ui/Layout.tsx
@@ -8,10 +8,10 @@ import Footer from '@/components/custom-ui/Footer'
 const Layout = () => {
   return (
     <ThemeProvider>
-      <div className="bg-primary-origin min-h-screen w-full">
+      <div className="bg-primary-origin w-full">
         <Header />
         <main className="bg-card mx-auto w-full max-w-lg p-4 pt-10 pb-30">
-          <div className="mx-auto min-h-screen w-full">
+          <div className="mx-auto min-h-[calc(100vh-226px)] w-full">
             <Outlet />
           </div>
         </main>
